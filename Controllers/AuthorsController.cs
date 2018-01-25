6a50ecf21e0b5d898ed73f3a020fb02f6002f0f6
Le Feng <lfung218@Les-MacBook-Air.local>
2018-01-24 19:05:53 -0500

    Added routing and controllers

---
+++ Controllers/AuthorsController.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fisher.Bookstore.Controllers
+{
+
+    public class AuthorsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("This is the Authors controller's index action");
+        }
+
+        public IActionResult Featured()
+        {
+            return Content("This is the Books controller's featured action");
+        }
+
+    }
+}
