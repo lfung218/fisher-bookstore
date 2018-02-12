64a1d5d75e6c1b3b664fde52dd497e03e24709e0
Le Feng <lfung218@Les-MacBook-Air.local>
2018-02-12 12:33:46 -0500

    Integrated Model with Views

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Fisher.Bookstore.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Fisher.Bookstore.Controllers
@@ -12,12 +13,39 @@ namespace Fisher.Bookstore.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            var books = GetBooks();
+            return View(books);
         }
 
         public IActionResult New()
         {
             return Content("This is the Books controller's new action");
+        }
+
+        private IEnumerable<Book> GetBooks()
+        {
+            var books = new List<Book>();
+
+            books.Add(new Book()
+            {
+                Title = "Continous Delivery",
+                Author = new Author() { Name = "Jez Humble"}
+            });
+            
+            books.Add(new Book()
+            {
+                Title = "Lean Enterprise",
+                Author = new Author() { Name = "Barry O'Reilly"}
+            });
+                        
+            books.Add(new Book()
+            {
+                Title = "The Goal",
+                Author = new Author() { Name = "Eliyahu Goldratt"}
+            });
+
+            return books;
+            
         }
 
     }
